eb725498fbdada2bfb9a9cf52ca4452c855f5ee7
justjo3l <comedyandcomputers@gmail.com>
2021-10-17 13:21:36 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# swiss-army-pocket-app
+ The Github Repository for the Swiss Army Pocket App
---
+++ android/app/src/main/kotlin/com/example/swiss_army_pocket_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.swiss_army_pocket_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
