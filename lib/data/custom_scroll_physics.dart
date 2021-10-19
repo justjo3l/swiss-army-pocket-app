d00dda39505feaa8b55b3d0a32a47c1f5e9b5905
justjo3l <comedyandcomputers@gmail.com>
2021-10-19 23:12:24 +0400

    Add ignore_for_file conditions to custom_scroll_physics.dart again
    Added ignore_for_file conditionds to custom_scroll_physics.dart again

--- lib/data/custom_scroll_physics.dart
+++ lib/data/custom_scroll_physics.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: annotate_overrides, overridden_fields, unused_import
+// ignore_for_file: annotate_overrides, overridden_fields, unused_import, must_be_immutable, prefer_typing_uninitialized_variables
 
 import 'package:flutter/material.dart';
 
