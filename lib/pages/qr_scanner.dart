a2de56fdd215d60455e847b2df7ffde0667a95d2
justjo3l <comedyandcomputers@gmail.com>
2021-10-26 18:19:02 +0400

    Add Placeholder details on QR Scanner page
    Added Placeholder details on QR Scanner page.

--- lib/pages/qr_scanner.dart
+++ lib/pages/qr_scanner.dart
@@ -10,7 +10,37 @@ class QrScannerScreenState extends State<QrScannerScreen> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      child: Scaffold(),
+      child: Scaffold(
+        body: Align(
+          child: Column(
+            children: [
+              Text(
+                'This doesn\'t work just yet!',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              Text(
+                'Give us a bit!',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                icon: Icon(Icons.arrow_back),
+                color: Theme.of(context).primaryColor,
+                iconSize: 30,
+              ),
+            ],
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+          ),
+          alignment: Alignment.center,
+        ),
+      ),
       onPanUpdate: (gestureDetails) {
         if (gestureDetails.delta.dy > 5) {
           Navigator.of(context).pop();
